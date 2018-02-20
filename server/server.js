818706f48ca4b548aba341d4a70581368c6e5ec9
malik <mdmalik.b@gmail.com>
2018-02-20 17:10:25 +0800

    Added user tokens

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const _ = require('lodash');
 
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require ('./models/todo');
 var {User} = require ('./models/user');
@@ -97,7 +98,20 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
 
+  var body = _.pick(req.body,['email','password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(404).send(e);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
